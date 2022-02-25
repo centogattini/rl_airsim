79c7af1e0b654c93e10599efb785a87d2744f8e7
steinspapillome <62851661+steinspapillome@users.noreply.github.com>
2022-02-25 20:18:18 +0300

    added roads for training and testing

---
+++ roads/rec_to_road.py
@@ -0,0 +1,14 @@
+import pandas as pd
+for model in ['dqn']:
+    data = pd.read_csv(f'airsim_recs/airsim_rec_{model}.txt',sep='\t')
+    x = data.POS_X
+    y = data.POS_Y
+    #x_ = []
+    #y_ = []
+    #for j in range(1,len(x),20):
+    #    x_.append(x[j])
+    #    y_.append(y[j])
+    xy_data=list(zip(x,y))
+    df = pd.DataFrame(xy_data)
+    df.to_csv(f'road_dots_{model}.csv')
+    print(df)
